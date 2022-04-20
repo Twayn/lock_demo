77abead00353757eb9d5e0c6e9a513f72cae750e
twayn <Twayn@ya.ru>
2022-04-20 18:15:02 +0300

    Completable future example

--- src/test/java/com/study/transactions/LockTest.java
+++ src/test/java/com/study/transactions/LockTest.java
@@ -9,19 +9,24 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
 import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.CyclicBarrier;
+import java.util.concurrent.Executor;
 import java.util.stream.Stream;
 
 import com.study.transactions.repositories.AccountRepository;
-import com.study.transactions.service.AccountServiceImpl;
+import com.study.transactions.service.AccountService;
 
+import static java.util.concurrent.CompletableFuture.delayedExecutor;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.stream.Collectors.toList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 @SpringBootTest
-@ActiveProfiles("h2-mem")
 public class LockTest {
     @Autowired
-    private AccountServiceImpl service;
+    private AccountService service;
 
     private static Long accountId = 0L;
     private static Boolean useLock = true;
@@ -60,6 +65,18 @@ public class LockTest {
         }
     };
 
+    private final Runnable depositForFuture = () -> {
+        for (int i = 0; i < 200; i++) {
+            service.depositOne(accountId, useLock);
+        }
+    };
+
+    private final Runnable withdrawForFuture = () -> {
+        for (int i = 0; i < 200; i++) {
+            service.withdrawOne(accountId, useLock);
+        }
+    };
+
     @Test
     public void withLockShouldNotLostAnyUpdate() {
         useLock = true;
@@ -86,7 +103,7 @@ public class LockTest {
             });
 
 
-        Assertions.assertEquals(0, service.getAccount(accountId, true).getAmount());
+        assertEquals(0, service.getAccount(accountId, true).getAmount());
     }
 
     @Test
@@ -116,5 +133,26 @@ public class LockTest {
 
 
         Assertions.assertNotEquals(0, service.getAccount(accountId, false).getAmount());
+    }
+
+    @Test
+    public void withCompletableFuture() {
+        useLock = false;
+
+        Executor delayed = delayedExecutor(1L, SECONDS);
+        final List<CompletableFuture<Void>> deposit = Stream
+            .generate(() -> CompletableFuture.runAsync(depositForFuture, delayed))
+            .limit(processors / 2)
+            .collect(toList());
+
+        final List<CompletableFuture<Void>> withdraw = Stream
+            .generate(() -> CompletableFuture.runAsync(withdrawForFuture, delayed))
+            .limit(processors / 2)
+            .collect(toList());
+
+        Stream.concat(deposit.stream(), withdraw.stream())
+            .forEach(CompletableFuture::join);
+
+        assertNotEquals(0, service.getAccount(accountId, true).getAmount());
     }
 }
